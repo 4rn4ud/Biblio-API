619881d61cbfb5cac56f2726821d1a974f0b267f
4rn4ud <94056904+4rn4ud@users.noreply.github.com>
2024-04-04 15:11:59 +0200

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/parkings', (req,res) => {
+    res.send("Liste des parkings")
+})
+
+// GET
+app.get('/tasks', async (req, res) => {
+    try {
+        const result = await pool.query("select * from tache");
+        res.send(result);
+    } catch (err) {
+        throw err;
+    }
+});
+  
+// POST
+app.post('/tasks', async (req, res) => {
+    let task = req.body;
+    try {
+        const result = await pool.query("insert into tache (titre, description) values (?, ?)", [task.titre, task.description]);
+        res.send('Nouvel tâche ajoutée avec succès !');
+    } catch (err) {
+        throw err;
+    }
+});
+  
+app.get('/',(req, res) => {
+    let render = `<form action="./tasks" method="post">
+    <p>Veuillez renseigner un titre et une description pour la nouvelle tâche :</p>
+    <div>
+        <label for="titre">Titre</label>
+        <input type="text" id="titre" name="titre" />
+
+        <label for="description">Description</label>
+        <input type="text" id="description" name="description" />
+    </div>
+    <div>
+        <button type="submit">Envoyer</button>
+    </div>
+    </form>`;
+    res.send(render);
+});
+
+
+
+// open database in memory
+let db = new sqlite3.Database('./db/biblio-bdd.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+});
+
+db.serialize(() => {
+  db.each(`SELECT id, nom, prenom
+           FROM Auteur`, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row.id + "\t" + row.nom + "\t" + row.prenom);
+  });
+});
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
